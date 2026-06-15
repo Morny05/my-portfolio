14adfcaab731b1ec5b68ea2e3e089910bb661a50
Morny05 <mohamedmorny01@gmail.com>
2026-06-15 12:11:23 +0100

    br

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -5,15 +5,15 @@ export default function About() {
 
     <section id="about" className="min-h-screen p-10"><br /><br />
       <h2 className="text-4xl font-bold text-center mb-4">About Me</h2><br />
-      <h5 className="text-gray-500 space-y-4 text-lg text-muted-foreground text-center max-w-3xl mx-auto">
+      <h5 className="text-gray-500 space-y-4 text-lg text-muted-foreground text-center max-w-3xl mx-auto"><br />
         Hello! I'm Mohamed Morny, a DevOps and Cloud enthusiast with a Master's degree in Big Data & Cloud Computing.
-        <br />
+        <br /><br />
         I have always been passionate about technology and its ability to solve real-world problems. My journey began with software development and gradually led me to the world of Cloud and DevOps, where I discovered my interest in automation, infrastructure management, and scalable systems.
-        <br />
+        <br /><br />
         Through internships and hands-on projects, I have gained practical experience with CI/CD pipelines, Docker, Kubernetes, Terraform, Linux, and cloud platforms such as AWS and GCP. I enjoy learning new technologies, tackling technical challenges, and building reliable solutions that improve development and deployment processes.
-        <br />
+        <br /><br />
         This website is where I share my projects, experiences, and learning journey in Cloud, DevOps, and software engineering. I am continuously expanding my skills and looking for opportunities to contribute to impactful projects while growing as a cloud professional.
-        <br />
+        <br /><br />
         Thank you for visiting, and I hope you enjoy exploring my work.
       </h5>
     </section>
